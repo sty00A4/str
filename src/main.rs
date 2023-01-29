3ca7201f828a957b657284d6d7adf3e745df998e
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2023-01-29 11:09:48 +0100

    run function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use std::{env, process::exit, io::{stdout, Write, stdin}, fs};
+use run::Program;
 
 mod error;
 mod lexer;
@@ -26,6 +27,19 @@ macro_rules! error_no_pos {
     };
 }
 
+fn run(program: &mut Program, path: &String, text: String) {
+    match lexer::lex(text.clone()) {
+        Ok(tokens) => match parser::parse(tokens) {
+            Ok(nodes) => match program.run(nodes) {
+                Ok(_) => println!("{}", program.stack),
+                Err(e) => { eprintln!("{}\n{}", program.stack, e.display_text(path, text)) }
+            }
+            Err(e) => { eprintln!("{}", e.display_text(path, text)) }
+        }
+        Err(e) => { eprintln!("{}", e.display_text(path, text)) }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut args = args.iter();
@@ -33,38 +47,20 @@ fn main() {
     match args.next() {
         Some(path) => match fs::read_to_string(path) {
             Ok(text) => {
-                let mut program = run::Program::std_program();
-                match lexer::lex(text.clone()) {
-                    Ok(tokens) => match parser::parse(tokens) {
-                        Ok(nodes) => match program.run(nodes) {
-                            Ok(_) => println!("{}", program.stack),
-                            Err(e) => { eprintln!("{}\n{}", program.stack, e.display_text(path, text)) }
-                        }
-                        Err(e) => { eprintln!("{}", e.display_text(path, text)) }
-                    }
-                    Err(e) => { eprintln!("{}", e.display_text(path, text)) }
-                }
+                let mut program = Program::std_program();
+                run(&mut program, path, text);
             }
             Err(e) => { eprintln!("error occurd while reading the file {path:?}: {e}"); exit(1) }
         }
         None => {
-            let mut program = run::Program::std_program();
+            let mut program = Program::std_program();
             let path = &"<stdin>".to_string();
             loop {
                 let mut input = String::new();
                 print!("> ");
                 let _ = stdout().flush();
                 let _ = stdin().read_line(&mut input);
-                match lexer::lex(input.clone()) {
-                    Ok(tokens) => match parser::parse(tokens) {
-                        Ok(nodes) => match program.run(nodes) {
-                            Ok(_) => println!("{}", program.stack),
-                            Err(e) => { eprintln!("{}\n{}", program.stack, e.display_text(path, input)) }
-                        }
-                        Err(e) => { eprintln!("{}", e.display_text(path, input)) }
-                    }
-                    Err(e) => { eprintln!("{}", e.display_text(path, input)) }
-                }
+                run(&mut program, path, input);
                 println!();
             }
         }
